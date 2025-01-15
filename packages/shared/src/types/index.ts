26b9ea40c718447b1801be983216ca4e35e1a04d
Anders Hafreager <andershaf@gmail.com>
2025-01-15 13:13:27 +0100

    Refactored into story

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,14 +1,1 @@
-export interface Page {
-  text: string;
-  illustrationBase64: string;
-}
-
-export interface Story {
-  id: string;
-  title: string;
-  summary?: string;
-  coverImageBase64: string;
-  pages: Page[];
-  createdAt?: Date;
-  isPreinstalled?: boolean;
-}
+export * from "./Story";
