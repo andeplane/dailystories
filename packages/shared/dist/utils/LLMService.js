ba323408ee15a8697b1c801b009838e694160b4c
Anders Hafreager <andershaf@gmail.com>
2025-01-15 01:03:06 +0100

    Removed dist folder and added to git ignore

--- packages/shared/dist/utils/LLMService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LLMService = void 0;
-class LLMService {
-}
-exports.LLMService = LLMService;
--- packages/shared/dist/utils/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { LLMService } from "./LLMService";
-export type { ChatMessage, ImageGenerationOptions } from "./LLMService";
-export { MixpanelService } from "./MixpanelService";
-export { OpenAIService } from "./OpenAIService";
-export { StoryGenerator } from "./StoryGenerator";
-export type { StorySettings } from "./StoryGenerator";
